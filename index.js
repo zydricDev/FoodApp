c9f797217a00e2148615f117083dfa180cd730a9
zydricDev <zydricw@gmail.com>
2020-11-13 02:36:42 -0500

    Test

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> console.log(`Server at port: ${PORT}`));
+
