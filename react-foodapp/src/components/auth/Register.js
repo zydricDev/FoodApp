70ea054f51e6fe32fd24e0d84011ba2a3dd05d13
zydricDev <zydricw@gmail.com>
2020-11-14 05:04:05 -0500

    Update

--- react-foodapp/src/components/auth/Register.js
+++ react-foodapp/src/components/auth/Register.js
@@ -2,37 +2,46 @@ import React, {useState, useContext} from 'react'
 import Axios from 'axios'
 import {useHistory} from 'react-router-dom'
 import UserContext from '../../context/UserContext'
+import ErrorNotice from '../misc/ErrorNotice'
 
 export default function Register() {
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
     const [passwordCheck, setPasswordCheck] = useState()
     const [displayName, setDisplayName] = useState()
+    const [error, setError] = useState()
 
     const {setUserData} = useContext(UserContext)
     const history = useHistory()
 
     const submit = async (e) =>{
         e.preventDefault()
-        const newUser = {email, password, passwordCheck, displayName}
-        await Axios.post('http://localhost:4000/users/register', newUser)
+        try{
+            const newUser = {email, password, passwordCheck, displayName}
+            await Axios.post('http://localhost:4000/users/register', newUser)
 
-        const loginRes = await Axios.post('http://localhost:4000/users/login',{
-            email, password
-        })
+            const loginRes = await Axios.post('http://localhost:4000/users/login',{
+                email, password
+            })
 
-        setUserData({
-            token: loginRes.data.token,
-            user: loginRes.data.user,
-        })
+            setUserData({
+                token: loginRes.data.token,
+                user: loginRes.data.user,
+            })
 
-        localStorage.setItem('auth-token', loginRes.data.token)
-        history.push('/')
+            localStorage.setItem('auth-token', loginRes.data.token)
+            history.push('/')
+        }catch(err){
+            err.response.data.msg && setError(err.response.data.msg)
+        }
     }
 
     return (
         <div>
             <h2>Register</h2>
+            {error && (
+                <ErrorNotice message={error} clearError={() => setError(undefined)}/>
+            )}
             <form onSubmit={submit}>
                 <label>Email</label>
                 <input id='register-email' type='email' onChange={e => setEmail(e.target.value)}/>
--- react-foodapp/src/components/pages/Home.js
+++ react-foodapp/src/components/pages/Home.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, {useEffect, useContext} from 'react'
+import {useHistory} from 'react-router-dom'
+import UserContext from '../../context/UserContext'
 
 export default function Home() {
+    const {userData} = useContext(UserContext)
+    const history = useHistory()
+
+    useEffect(()=>{
+        if(!userData.user) history.push('/login');
+
+    })
+
     return (
         <div>
             Home
