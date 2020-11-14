4452fd1d4b523060d2b76e89d7205c55f7d8842f
zydricDev <zydricw@gmail.com>
2020-11-13 22:02:17 -0500

    Base

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-const app = express();
+
 app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+    (err) =>{
+        if(err) throw err;
+        console.log('connected to db');
+    }
+);
+
 app.listen(PORT, ()=> console.log(`Server at port: ${PORT}`));
 
+app.use('/users',require('./routes/userRouter'));
---
+++ routes/userRouter.js
@@ -0,0 +1,100 @@
+const router = require('express').Router();
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const auth = require('../middleware/auth');
+
+router.post('/register', async (req,res)=>{
+    try{
+        let {email, password, passwordCheck, displayName} = req.body;
+        if(!email || !password || !passwordCheck){
+            return res.status(400).json({msg: "Not all field have been entered"});
+        }
+        if(password.length < 5){
+            return res.status(400).json({msg: "Password is less than 5 characters"});
+        }
+        if(password !== passwordCheck){
+            return res.status(400).json({msg: "Same password is needed"});
+        }
+        const existingUser = await User.findOne({email: email});
+        if(existingUser){
+            return res.status(400).json({msg: "Email exist"});
+        }
+        if(!displayName){
+            displayName = email;
+        }
+        const salt = await bcrypt.genSalt();
+        const hashedPass = await bcrypt.hash(password, salt);
+        const newUser = new User({
+            email,
+            password: hashedPass,
+            displayName
+        });
+        const savedUser = await newUser.save();
+        res.json(savedUser);
+
+        
+    }
+    catch(err){
+        res.status(500).json({error: err.message});
+    }
+});
+
+router.post('/login', async (req,res)=>{
+    try{
+        const{email, password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({msg: "Not all field have been entered"});
+        }
+        const user = await User.findOne({email: email});
+        if(!user){
+            return res.status(400).json({msg: "Not account with this email registered"});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({msg: "Invalid password"});
+        }
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+        res.json({
+            token,
+            user: {
+                id: user._id,
+                displayName: user.displayName,
+                email: user.email,
+            }
+        })
+    }catch(err){
+        res.status(500).json({error: err.message});
+    }
+});
+
+router.delete('/delete', auth, async (req,res) =>{
+    try{
+        const deletedUser = await User.findByIdAndDelete(req.user);
+        res.json(deletedUser);
+
+    }catch(err){
+        res.status(500).json({error: err.message});
+    }
+});
+
+router.post('/tokenIsValid', async (req,res)=>{
+    try{
+        const token = req.header('auth-token');
+        if(!token){
+            return res.json(false);
+        }
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        if(!verified){
+            return res.json(false);
+        }
+        const user = await User.findById(verified.id);
+        if(!user){
+            return res.json(false);
+        }
+        return res.json(true);
+    }catch(err){
+        res.status(500).json({error: err.message});
+    }
+});
+module.exports = router;
