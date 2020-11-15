b624012ef25495a8e682c04400c50979205fbce8
zydricDev <zydricw@gmail.com>
2020-11-14 23:12:31 -0500

    Update

--- react-foodapp/src/components/misc/ErrorNotice.js
+++ react-foodapp/src/components/misc/ErrorNotice.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function ErrorNotice(props) {
     return (
-        <div>
-            <span>{props.message}</span>
-            <button onClick={props.clearError}>X</button>
+        <div className='bg-red-t-75'>
+            <span className='mx-3'>{props.message}</span>
+            <button className='bg-red-t-75 displ hover:text-white p-1 focus:outline-none' onClick={props.clearError}>X</button>
         </div>
     )
 }
