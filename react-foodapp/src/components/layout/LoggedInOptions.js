300d8cedbaf35a2a0cd7e3a8ff1aca3614635d59
zydricDev <zydricw@gmail.com>
2020-11-17 23:30:40 -0500

    Update

--- react-foodapp/src/components/layout/LoggedInOptions.js
+++ react-foodapp/src/components/layout/LoggedInOptions.js
@@ -8,14 +8,21 @@ export default function LoggedInOptions() {
     const history = useHistory();
     
     const foodRegister = () => history.push('/food/register')
+    const myStore = () => history.push('/food/myStore')
     
     return (
         <nav className='flex'>
             {
                 userData.user ? (
-                    <div className='p-3 flex border-l border-black'>
-                        <button className='inline-block mx-3 hover:text-white focus:outline-none' onClick={foodRegister}>Add Food</button>
-                    </div>
+                    <>
+                        <div className='p-3 flex border-l border-black'>
+                            <button className='inline-block mx-3 hover:text-white focus:outline-none' onClick={foodRegister}>Add Food</button>
+                        </div>
+                        <div className='p-3 flex border-l border-black'>
+                            <button className='inline-block mx-3 hover:text-white focus:outline-none' onClick={myStore}>View Shop</button>
+                        </div>
+                    </>
+                    
                 ) : (
                     <div className='p-3 flex border-l border-black'> 
                         <button className='inline-block mx-3 hover:text-white focus:outline-none'>Does something</button>
