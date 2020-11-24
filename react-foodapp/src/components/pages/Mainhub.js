bb519ac040f517590d8b65d4ce86c73857de9022
zydricDev <zydricw@gmail.com>
2020-11-24 06:50:18 -0500

    Mainhub edit

---
+++ react-foodapp/src/components/pages/Mainhub.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function Mainhub() {
+    return (
+        <div className='flex'>
+            <div className='flex w-2/6 border-r'>
+                <div className='flex-col w-full'>
+                    <div className='flex p-5 align-baseline'>
+                        <p className='font-bold text-2xl'>Filters</p>
+                        <p className='ml-3 p-2'>Clear all</p>
+                    </div>
+
+                    <div className='p-5 flex justify-center'>
+                        <p className='hover:border-0 hover:bg-blue-600 hover:text-white w-full text-center py-2 rounded-tl-lg rounded-bl-lg text-blue-500 font-medium border border-gray-400'>ASDASD</p>
+                        <p className='hover:border-0 hover:bg-blue-700 hover:text-white w-full text-center py-2 rounded-tr-lg rounded-br-lg text-blue-500 font-medium border border-gray-400'>abasd</p>
+                    </div>
+
+                    <p className='p-5'>ASDASD</p>
+                </div>
+            </div>
+
+            <div className="flex-col w-full">
+                <div className='flex w-full'>
+                    
+                </div>
+
+                <div className="flex-col w-full">
+
+                </div>
+            </div>
+        </div>
+    )
+}
