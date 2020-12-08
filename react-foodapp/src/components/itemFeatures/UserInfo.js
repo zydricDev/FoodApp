78d140353bf8bf9d98b70790096d01dae9199120
zydricDev <zydricw@gmail.com>
2020-12-08 03:40:16 -0500

    fixed size

--- react-foodapp/src/components/itemFeatures/UserInfo.js
+++ react-foodapp/src/components/itemFeatures/UserInfo.js
@@ -18,24 +18,24 @@ export default function UserInfo(propUrl) {
     if(usrInfo.data && usrRating.data){
         let ratings = usrRating.data.rating
         content = 
-        <div className='px-10 py-5 w-full flex items-center'>
-            <div className='w-3/6'>
-                <div className='px-5 text-3xl font-medium text-white'>
+        <div className='px-5 lg:px-10 py-5 w-full lg:flex items-center'>
+            <div className='lg:w-3/6'>
+                <div className='lg:px-5 text-3xl font-medium text-white'>
                     <p>{usrInfo.data.displayName}</p>
                 </div>
-                <div className='flex justify-between px-5 items-center gap-3'>
-                    <div className='w-5/6'>
+                <div className='grid grid-cols-1 lg:flex lg:justify-between lg:px-5 lg:items-center gap-3'>
+                    <div className='lg:w-5/6'>
                         <p className='text-white break-all'>{usrInfo.data.address}, {usrInfo.data.zipcode}</p>
                         <p className='border-b border-t border-gray-400 py-2 text-white'>
                             Phone: ({usrInfo.data.phone.substring(0,3)})-{usrInfo.data.phone.substring(3,6)}-{usrInfo.data.phone.substring(6,10)}
                         </p>
                     </div>
-                    <div className='w-5/6 px-5'>
-                        <img src={usrInfo.data.icon} alt={usrInfo.data.icon} className='h-40 w-40 object-cover'/>
+                    <div className='lg:w-5/6 lg:px-5'>
+                        <img src={usrInfo.data.icon} alt={usrInfo.data.icon} className='w-full h-40 lg:w-40 object-cover'/>
                     </div>
                 </div>
             </div>
-            <div className='flex-col'>
+            <div className='mt-5 lg:m-0 flex-col'>
                 <div className='mb-2 text-2xl text-white font-medium'>
                     <p>Reviews for {usrInfo.data.displayName}</p>
                 </div>
