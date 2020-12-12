c1cfa434cd1abf33aa5df586f762ee05bf3f9159
zydricDev <zydricw@gmail.com>
2020-12-12 05:48:54 -0500

    updated map

--- react-foodapp/src/components/itemFeatures/MapDisplay.js
+++ react-foodapp/src/components/itemFeatures/MapDisplay.js
@@ -1,10 +1,94 @@
 import React, { useEffect, useRef, useState } from 'react'
-
-import mapboxgl from 'mapbox-gl'
-import mapboxtoken from '../../mapboxtoken'
-import axios from 'axios'
+import { useAxiosGet } from '../../Hooks/HttpRequest'
+import ReactMapGL, { Marker, Popup } from 'react-map-gl'
 import Loader from '../misc/Loader'
 
+
+export default function MapDisplay(props) {
+    const address = useRef(props.address)
+    const name = useRef(props.name)
+    const phone = useRef(props.phone)
+    const icon = useRef(props.icon)
+    const [selected, setSelected] = useState(false)
+
+    let content = <Loader></Loader>
+    const [viewPort, setViewPort] = useState({
+        latitude: undefined,
+        longitude: undefined,
+        width: '100vw',
+        height: '10vh',
+        zoom: 14
+
+    })
+    const coor = useAxiosGet(props.url)
+    
+    if(coor.error){
+        content = <Loader></Loader>
+    }
+    useEffect(()=>{
+        if(coor.data){
+            if(!coor.data.msg){
+                const loadData = () =>{
+                    setViewPort({
+                        latitude: coor.data.coordinates.lat,
+                        longitude: coor.data.coordinates.lng,
+                        width: '100%',
+                        height: '20em',
+                        zoom: 14
+                    })
+                }
+                loadData()
+            }
+            
+        }
+ 
+    },[coor.data])
+    
+    if(coor.data){
+        if(!coor.data.msg){
+            content = 
+            <>
+                <ReactMapGL
+                    {...viewPort}
+                    mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+                    mapStyle={process.env.REACT_APP_MAPBOX_STYLE}
+                    onViewportChange={viewPort =>{
+                        setViewPort(viewPort)
+                    }}
+                    
+                >
+                <button onClick={()=>{setSelected(!selected)}}>
+                    <Marker latitude={coor.data.coordinates.lat} longitude={coor.data.coordinates.lng} offsetLeft={-10} offsetTop={-35}>
+                        <img src='https://upload.wikimedia.org/wikipedia/commons/d/d1/Google_Maps_pin.svg' alt={name.current}/>
+                    </Marker>
+                </button>
+                
+                {selected && 
+                <Popup latitude={coor.data.coordinates.lat} longitude={coor.data.coordinates.lng} onClose={()=>{setSelected(false)}} offsetLeft={20}>
+                    <div className='px-5 text-center w-40'>
+                        <img src={icon.current} className='w-full h-20 object-cover' alt={name.current}/>
+                        <p className='font-medium text-xl'>{name.current}</p>
+                        <p className='font-medium'>{address.current}</p>
+                        <p className='font-medium'>{phone.current}</p>
+                    </div>
+                </Popup>}
+                </ReactMapGL>     
+            </>
+        }else{
+            content = null
+        }
+        
+    }
+
+    return (
+        content
+    )
+}
+
+
+
+
+/*
 mapboxgl.accessToken = mapboxtoken
 
 
@@ -55,14 +139,7 @@ export default function MapDisplay(props) {
                     
                     
                 })
-                .catch(err =>{
-                    if (axios.isCancel(err)) {
-                        console.log('Request canceled', err.message);
-                    }
-                })
-                
-                
-                
+        
             }catch(err){
                 if (axios.isCancel(err)) {
                     console.log('Request canceled', err.message);
@@ -80,7 +157,7 @@ export default function MapDisplay(props) {
             setMount(false)
             source.cancel('Unmounted')
         }
-    }, [props.url])
+    }, [props])
 
    
     if(mount){
@@ -94,4 +171,6 @@ export default function MapDisplay(props) {
         content
     )
 }
+
+*/
 
