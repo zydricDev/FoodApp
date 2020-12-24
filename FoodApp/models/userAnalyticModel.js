08df1777e968824b2babb4c743fe4f364f41d893
zydricDev <zydricw@gmail.com>
2020-12-23 22:52:53 -0500

    preanalytics

--- FoodApp/models/userAnalyticModel.js
+++ FoodApp/models/userAnalyticModel.js
@@ -3,14 +3,13 @@ const mongoose = require('mongoose');
 
 const userAnalyticSchema = new mongoose.Schema({
     item_id: {type: String, required: true},
-    item_name: {type: String},
     item_price: {type: String},
     client_id: {type: String, required: true},
     client_geo_lat: {type: String},
     client_geo_lng: {type: String},
     seller_id: {type: String, required: true},
-    seller_name: {type: String},
     quantity: {type: String},
+    distance: {type: String},
     entry_date: {type: Date, default: new Date()}
 })
 
